fc4e3daf25fb55777f364f3ca679c1c8c7f935c4
vinishasaratker <vinishasaratker@gmail.com>
2025-08-29 16:41:18 +0530

    hyyy

---
+++ loops/basicloop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // loops are using for performing  repetative task 
+    // there are three types of loops for/ ehile/ do while
+    //printingof even number
+    // for(int i=1;i<=10;i++){
+    //     if(i%2==0){
+    //         cout<<i<<"  ";
+    //     }
+    // }
+    //print odd numbers
+    // for(int i=1; i<=10;i++){
+    //     if(i%2!=0){
+    //         cout<<i<<" ";
+    //     }
+    // }
+    
+    
+   
+}
