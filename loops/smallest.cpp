bd1881242ee50d8f369e03ea647658e9d72aeddd
vinishasaratker <vinishasaratker@gmail.com>
2025-08-30 11:39:26 +0530

    new edits

---
+++ loops/smallest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    int num;
+    cout<<"enter any number";
+    cin>>num;
+    int value;
+    while(num>0){
+        int digit=num%10;
+
+        i
+    }
+ }
