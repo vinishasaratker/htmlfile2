7f4edad820c168898a5287ee8069d4a4f8565e72
vinishasaratker <vinishasaratker@gmail.com>
2025-08-30 15:01:34 +0530

    laptop

---
+++ loops/greatestandsmallestinadigit.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+ int main()//{
+//     int number;
+//     cout<<"enter any number";
+//     cin>>number;
+//     int value=0;
+//     int digit;
+//     while(number>0){
+//          int digit =number%10;
+//         if(digit>value){
+//             value=digit;
+//         }
+        
+//         number=number/10;
+ 
+//     }
+//     cout<<"greatest digit is :"<<value;
+// }
+
+
+// {
+//     int num;
+//     cout<<"enterany number";
+//     cin>>num;
+//     int value=9;
+//     while(num>0){
+//         int digit=num%10;
+//         if(digit<value){
+// value=digit;
+//         }
+//         num=num/10;
+//     }
+//     cout<<"smallest digit is :"<<value;
+// }
+// {
+//  //reverse of any number
+//  int num ,digit;
+//  int rev=0;
+//  cout<<"enter any number";
+//  cin>>num;
+//  while(num>0){
+//     int digit=num%10;
+// rev=rev*10+digit;
+//     num=num/10;
+//  }
+// cout<<rev;
+
+// }
+
+{
+    int num , digit,temp;
+    cout<<"enter num";
+    cin>>num;
+    temp=num;
+    int rev=0;
+    while(num>0)
+    {
+int digit=num%10;
+rev=rev*10+digit;
+num=num/10;
+    }
+    if(temp==rev){
+    cout<<"palidrom";
+}
+else{
+    cout<<"not a palidrom";
+}
+}
--- loops/sumofdigit.cpp
+++ loops/sumofdigit.cpp
@@ -1,33 +1,20 @@
 #include<iostream>
 using namespace std;
- int main()//{
-//      int num;
-// cout<<" enter number";
-// cin>>num;
-// int sum=0;
-// while(num>0){
-//     int digit=num%10;
-//     sum=sum+digit;
-//     num=num/10;
-
-// }
-// cout<<sum;
-// }
-// {
+ int main()
+{
 // even number in a digit
-//     int num;
-//     cout<<"enter any number";
-//     cin>>num;
-//     while(num>0){
-//         int digit=num%10;
+   int num;  
+     cout<<"enter any number";
+    cin>>num;
+ while(num>0){
+         int digit=num%10;
        
-//         if(digit%2==0){
-//             cout<<digit;
-
-//         }
-//          num=num/10;
-//     }
-// }
+        if(digit%2==0){
+           cout<<digit;
+       }
+         num=num/10;
+    }
+ }
 // {
 //     // only odd number in a digit
 //    int num;
@@ -43,24 +30,3 @@ using namespace std;
 //          num=num/10;
 //     }
 // }
-{
-    // greatest number in a digit
-    int num;
-    cout<<"enter any number";
-    cin>>num;
-     int var=0;
-     int digit;
-    while(num>0){
-        int digit=num%10;
-        
-        if(digit>var){
-            var=digit;
-            // cout<<var;
-        }
-        num=num/10;
-        //  cout<<digit;
-    }
-    cout<<digit;
-    
-   
-}
