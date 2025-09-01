7862af760a36d0edd93474d8c03c07f131deda60
vinishasaratker <vinishasaratker@gmail.com>
2025-09-01 21:42:25 +0530

    bool

--- loops/sumofdigit.cpp
+++ loops/sumofdigit.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
  int main()
-{
-// even number in a digit
-   int num;  
-     cout<<"enter any number";
-    cin>>num;
- while(num>0){
-         int digit=num%10;
+// {
+// // even number in a digit
+//    int num;  
+//      cout<<"enter any number";
+//     cin>>num;
+//  while(num>0){
+//          int digit=num%10;
        
-        if(digit%2==0){
-           cout<<digit;
-       }
-         num=num/10;
-    }
- }
+//         if(digit%2==0){
+//            cout<<digit;
+//        }
+//          num=num/10;
+//     }
+//  }
 // {
 //     // only odd number in a digit
 //    int num;
@@ -30,3 +30,15 @@ using namespace std;
 //          num=num/10;
 //     }
 // }
+{
+   int num;
+   cout<<"enter num";
+   cin>>num;
+   int sum=0;
+   while(num>0){
+int digit=num%10;
+sum=sum+digit;
+num=num/10;
+   }
+   cout<<sum;
+}
