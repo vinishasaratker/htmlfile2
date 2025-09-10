39ff7e7ca48e0eca894d4646db935fcbc8a777b2
vinishasaratker <vinishasaratker@gmail.com>
2025-09-11 00:31:57 +0530

    put

---
+++ queall.cpp/tricky.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+    // if (NULL)                     // NULL is 0 → false
+    //     cout << "NULL";
+    // else if (false)               // lowercase 'false' is predefined boolean
+    //     cout << "TRUE";
+    // else
+    //     cout << "FALSE";          // This will execute
+
+    // return 0;
+
+    // int i=0;
+    // int j=0;
+    // if(i++==j++){
+    //     cout<<i--<<j--;//pehle 1or 1 print ho jayenga fir increase honga
+    // }
+    // else{
+    //     cout<<i<<j;
+    // }
+//  {
+//     int i = 0, j = 1, k = 0;
+    
+//     if (++k, j, i++)  // Comma operator is used
+//         cout << i << j << k;
+//     // no output
+//     return 0;
+// }
+{
+    int i;
+    if(true){
+        cout<<"this will work";
+
+    }
+    else{
+        cout<<" not work ";
+    }
+}
+
